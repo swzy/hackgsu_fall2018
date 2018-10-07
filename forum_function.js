4ffab92cabc13d80d4837952e73871af14626c3b
kimberlymai18 <43930484+kimberlymai18@users.noreply.github.com>
2018-10-07 09:40:48 -0400

    Add files via upload

---
+++ forum_function.js
@@ -0,0 +1,8 @@
+function transferInput()
+{
+   var x = document.getElementById("title").value;
+   var y = document.getElementById("textarea").value;
+   document.getElementById("textbox").value = x;
+   document.getElementById("titleTest2").value = y;
+
+};
